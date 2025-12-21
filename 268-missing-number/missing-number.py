2fb5552ab1c4b7eb1bf6e25d502366aef44d9d90
vinod-gtec <144086586+vinod-gtec@users.noreply.github.com>
2025-12-21 22:47:52 +0530

    Time: 2293 ms (21.22%) | Memory: 13.4 MB (31.73%) - LeetSync

---
+++ 268-missing-number/missing-number.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        siz=len(nums)
+        i=0
+        while(i<siz):
+            if i not in nums:
+                a=i
+                return a
+            i=i+1
+        return len(nums)
+    
+        
