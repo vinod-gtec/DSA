5619a662c3cf7b52400550c3e9a92ac288ed105f
vinod-gtec <144086586+vinod-gtec@users.noreply.github.com>
2025-12-22 23:11:25 +0530

    Time: 12 ms (90.46%) | Memory: 13.6 MB (68.54%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.py
+++ 485-max-consecutive-ones/max-consecutive-ones.py
@@ -10,14 +10,12 @@ class Solution(object):
         j=0
         if len(nums)==0:
             return 0
-        while(j<len(nums)):
-            if(nums[j]==1):
-                j=j+1
+        for num in nums:
+            if num:
                 count=count+1
                 if(count>max1):
                     max1=count
             else:
-                j=j+1
                 count=0
         return max1
         
