0776ae340a7def009a96e86caea08b0204466154
vinod-gtec <144086586+vinod-gtec@users.noreply.github.com>
2025-12-27 17:11:06 +0530

    Time: 33 ms (89.62%) | Memory: 21.2 MB (81.70%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -5,21 +5,20 @@ class Solution(object):
         :rtype: int
         """
         a=len(nums)
-        maxi=nums[0]
         summ=0
+        maxx=nums[0]
         astart=-1
-        send=-1
+        aend=-1
 
         for i in range(a):
             start=i
             summ=summ+nums[i]
-            if(summ>maxi):
-                maxi=summ
+            if (summ>maxx):
+                maxx=summ
                 start=astart
-                send=i
-
-            if(summ<=0):
+                aend=i
+            if summ<=0:
                 summ=0
-        return maxi
-
-
+        return maxx
+        
+            
