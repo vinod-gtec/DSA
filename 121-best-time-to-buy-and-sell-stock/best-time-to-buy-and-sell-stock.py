532925d422ffad53480080b1a9d684cf8c97fa1a
vinod-gtec <144086586+vinod-gtec@users.noreply.github.com>
2025-12-27 23:11:03 +0530

    Time: 48 ms (72.31%) | Memory: 19.4 MB (34.31%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -3,10 +3,12 @@ class Solution(object):
         mi=prices[0]
         profit=0
 
-        for p in prices:
-            if p<mi:
-                mi=p
-            else:
-                profit=max(profit,p-mi)
+        for i in range(1,len(prices)):
+            if prices[i]<mi:
+                mi=prices[i]
+            elif prices[i]-mi>profit:
+                profit=prices[i]-mi
         return profit
+
+            
 
