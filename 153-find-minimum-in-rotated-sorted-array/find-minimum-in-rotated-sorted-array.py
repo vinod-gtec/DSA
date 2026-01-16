da506ba5d427867f636801dd01b009a81abe0bbc
vinod-gtec <144086586+vinod-gtec@users.noreply.github.com>
2026-01-16 18:39:51 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (94.17%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -4,15 +4,5 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        lo=0
-        high=len(nums)-1
-        ans=10*100
-        while(lo<=high):
-            mid=(lo+high)//2
-            if(nums[lo]<=nums[mid]):
-                ans=min(ans,nums[lo])
-                lo=mid+1
-            else:
-                ans=min(ans,nums[mid])
-                high=mid-1
-        return ans
+        a=min(nums)
+        return a
