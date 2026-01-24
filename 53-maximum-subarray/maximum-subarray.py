c9a3f1b862617d38548820bc9ed2392341a3815f
vinod-gtec <144086586+vinod-gtec@users.noreply.github.com>
2026-01-25 01:07:26 +0530

    Time: 37 ms (88.02%) | Memory: 21.4 MB (48.32%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -4,21 +4,25 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        a=len(nums)
         summ=0
-        maxx=nums[0]
+        a=len(nums)
         astart=-1
         aend=-1
-
+        maxx=nums[0]
+        
         for i in range(a):
             start=i
             summ=summ+nums[i]
-            if (summ>maxx):
+            if summ>maxx:
                 maxx=summ
                 start=astart
                 aend=i
-            if summ<=0:
+            if(summ<0):
                 summ=0
-        return maxx
+        return maxx 
+
+            
+
+
         
             
