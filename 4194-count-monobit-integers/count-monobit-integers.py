de6760747fe5650a6b447af82a6e6d139085393c
vinod-gtec <144086586+vinod-gtec@users.noreply.github.com>
2026-02-02 00:30:52 +0530

    Time: 363 ms (5.09%) | Memory: 12.4 MB (89.81%) - LeetSync

--- 4194-count-monobit-integers/count-monobit-integers.py
+++ 4194-count-monobit-integers/count-monobit-integers.py
@@ -4,36 +4,16 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        res = [0]
+        i=2
         if n==0:
             return 1
         if n==1:
             return 2
-
-        for i in range(1, n + 1):
-            num = 0
-            place = 1
-            x = i
-            while x > 0:
-                num += (x % 2) * place
-                place *= 10
-                x //= 2
-            res.append(num)
-
-        a=[]
-        for i in res:
-            a.append(str(i))
-        b=[]
-        count=0
-        for i in range(len(a)):
-            is_mono=True
-            for j in range(len(a[i])):
-                if(a[i][j]!=a[i][0]):
-                    is_mono=False                
-                    break
-            if is_mono:
+        count=2
+        while(i<n):
+            if (2**i)-1<=n:
                 count=count+1
-
+            i=i+1
         return count
 
 
