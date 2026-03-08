a2dbbe5072a67776ce124613dd5a66c46f33a125
vinod-gtec <144086586+vinod-gtec@users.noreply.github.com>
2026-03-08 15:05:55 +0530

    Time: 18 ms (19.99%) | Memory: 12.4 MB (98.52%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
@@ -6,9 +6,15 @@ class Solution(object):
         :rtype: List[int]
         """
         a=[]
-        for i in nums1:
-            if i in nums2:
-                a.append(i)
-                nums2.remove(i)
+        if len(nums1)<len(nums2):
+            for i in nums1:
+                if i in nums2:
+                    a.append(i)
+                    nums2.remove(i)
+        else:
+            for i in nums2:
+                if i in nums1:
+                    a.append(i)
+                    nums1.remove(i)
         return a            
         
