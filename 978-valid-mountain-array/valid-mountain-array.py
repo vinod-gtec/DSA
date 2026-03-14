9c3bf9e180bcaec4fcc5924cdb4bf84b5aa5b2a6
vinod-gtec <144086586+vinod-gtec@users.noreply.github.com>
2026-03-14 14:41:21 +0530

    Time: 155 ms (70.97%) | Memory: 13.6 MB (98.92%) - LeetSync

---
+++ 978-valid-mountain-array/valid-mountain-array.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def validMountainArray(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: bool
+        """
+        count=0
+        dip=0
+        if len(arr)<=2:
+            return False
+        j=0
+        for i in range(len(arr)-1):
+            if arr[i]<arr[i+1]:
+                j=j+1
+                pass
+            else:
+                count=count+1
+                break
+        for i in range(j,len(arr)-1):
+            if arr[i]>arr[i+1]:
+                pass
+            else:
+                count=count+1
+
+        if count==1 and j>0:
+            return True
+        return False
