b1243717cd013866213d69167996b39cddc98705
vinod-gtec <144086586+vinod-gtec@users.noreply.github.com>
2026-03-15 22:41:02 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (52.36%) - LeetSync

--- 172-factorial-trailing-zeroes/factorial-trailing-zeroes.py
+++ 172-factorial-trailing-zeroes/factorial-trailing-zeroes.py
@@ -4,18 +4,10 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        a=str(factorial(n))
-        len_a=len(a)
         count=0
-        for i in range(len_a-1,-1,-1):
-            if a[i]=="0":
-                count=count+1
-            else:
-                break
+        div=5
+        while(n>=div):
+            count=count + n//div
+            div=div*5
         return count
-    
-    def factorial(self,n):
-        if n==1:
-            return n
-        return n*factorial(n-1)
         
