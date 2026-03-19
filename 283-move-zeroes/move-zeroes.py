d61314f03ec85ca2903b9b04b34385c6c41dd3ce
vinod-gtec <144086586+vinod-gtec@users.noreply.github.com>
2026-03-19 12:31:24 +0530

    Time: 8 ms (42.96%) | Memory: 13.5 MB (83.52%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -4,21 +4,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        if len(nums)==1:
-            return nums
-        n=len(nums)
         i=0
-        j=1
+        j=0
+        n=len(nums)
         while(j<n):
-            if nums[i]==0 and nums[j]!=0:
-                temp=nums[i]
-                nums[i]=nums[j]
-                nums[j]=temp
+            if nums[j]!=0:
+                temp=nums[j]
+                nums[j]=nums[i]
+                nums[i]=temp
                 i=i+1
-                j=j+1
-            elif nums[i]!=0:
-                i=i+1
-                j=j+1
-            else:
-                j=j+1
+            j=j+1
         return nums
