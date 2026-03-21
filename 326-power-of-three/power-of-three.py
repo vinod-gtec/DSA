c69cc89b90dcd26ad25998d238b8278c892be88f
vinod-gtec <144086586+vinod-gtec@users.noreply.github.com>
2026-03-21 22:52:07 +0530

    Time: 7 ms (81.13%) | Memory: 12.3 MB (75.78%) - LeetSync

--- 326-power-of-three/power-of-three.py
+++ 326-power-of-three/power-of-three.py
@@ -4,14 +4,13 @@ class Solution(object):
         :type n: int
         :rtype: bool
         """
-        summ=3
-        i=1
-        if n==1:
+        summ=n
+        i=0
+        if n<1:
+            return False
+        while(summ%3==0):
+            summ=summ//3
+        if summ==1:
             return True
-        while(summ<=n):
-            if 3**i==n:
-                return True
-            else:
-                i=i+1
-                summ=3**i
         return False
+    
