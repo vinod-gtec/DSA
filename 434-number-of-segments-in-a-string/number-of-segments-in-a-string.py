6c58f0ba3e15dfbd416fc64b046e0675a760f2e3
vinod-gtec <144086586+vinod-gtec@users.noreply.github.com>
2026-04-02 22:43:44 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (69.87%) - LeetSync

---
+++ 434-number-of-segments-in-a-string/number-of-segments-in-a-string.py
@@ -0,0 +1,10 @@
+import string
+class Solution(object):
+    def countSegments(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        return len(s.split())
+
+        
