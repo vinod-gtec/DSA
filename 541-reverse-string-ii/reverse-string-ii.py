274ddd5a9adc3c54f592513d9120b60ad99921ea
vinod-gtec <144086586+vinod-gtec@users.noreply.github.com>
2026-04-13 23:58:35 +0530

    Time: 3 ms (55.11%) | Memory: 12.7 MB (18.37%) - LeetSync

--- 541-reverse-string-ii/reverse-string-ii.py
+++ 541-reverse-string-ii/reverse-string-ii.py
@@ -1,20 +1,15 @@
 class Solution(object):
     def reverseStr(self, s, k):
-        """
-        :type s: str
-        :type k: int
-        :rtype: str
-        """
-        s=list(s)
-        a=""
-        for i in range(0,len(s),2*k):
-            temp=s[i:k+i]
-            val=temp[::-1]
-            s[i:k+i]=val
+        s = list(s)
+        n = len(s)
 
-        a=""
-        for i in s:
-            a=a+i 
-        return a
+        for start in range(0, n, 2 * k):
+            left = start
+            right = min(start + k - 1, n - 1)
 
-        
+            while left < right:
+                s[left], s[right] = s[right], s[left]
+                left += 1
+                right -= 1
+
+        return "".join(s)
