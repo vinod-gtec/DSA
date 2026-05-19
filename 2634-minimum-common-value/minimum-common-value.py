dda3cdf1d2f36daba1b05673aa4483ced5c05035
vinod-gtec <144086586+vinod-gtec@users.noreply.github.com>
2026-05-19 23:01:10 +0530

    Time: 13 ms (59.32%) | Memory: 24.2 MB (58.47%) - LeetSync

---
+++ 2634-minimum-common-value/minimum-common-value.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def getCommon(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: int
+        """
+        n1=len(nums1)
+        n2=len(nums2)
+        i=0
+        j=0
+        while(i<n1 and j<n2):
+            if nums1[i]==nums2[j]:
+                return nums1[i]
+            elif nums1[i]<nums2[j]:
+                i=i+1
+            else:
+                j=j+1
+        return -1
+        
