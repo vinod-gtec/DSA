2ade6e42560f0149910b8e05b1397803dd054a8c
vinod-gtec <144086586+vinod-gtec@users.noreply.github.com>
2026-06-09 22:50:59 +0530

    Time: 32 ms (11.76%) | Memory: 16.2 MB (70.59%) - LeetSync

---
+++ 4005-maximum-total-subarray-value-i/maximum-total-subarray-value-i.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def maxTotalValue(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        return (max(nums)-min(nums))*k
+        
