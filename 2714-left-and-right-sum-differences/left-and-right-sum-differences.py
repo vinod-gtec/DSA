09f375fc2d603c4d90f45a5d2e3eb53d53068135
vinod-gtec <144086586+vinod-gtec@users.noreply.github.com>
2026-06-10 23:50:42 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (80.36%) - LeetSync

--- 2714-left-and-right-sum-differences/left-and-right-sum-differences.py
+++ 2714-left-and-right-sum-differences/left-and-right-sum-differences.py
@@ -4,27 +4,15 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        left_sum=[]
-        right_sum=[]
-        for i in range(len(nums)):
-            temp1=nums[0:i]
-            if len(temp1)<=0:
-                left_sum.append(0)
-            else:
-                see=sum(temp1)
-                left_sum.append(see)
-        for i in range(len(nums)-1,-1,-1):
-            temp1=nums[len(nums):i:-1]
-            if len(temp1)<=0:
-                right_sum.append(0)
-            else:
-                see=sum(temp1)
-                right_sum.append(see)
-        right_sum=right_sum[::-1]
-        ne=[]
-        for i in range(len(nums)):
-            ne.append(abs(left_sum[i]-right_sum[i]))
-        return ne
+        total = sum(nums)
+        left = 0
+        ans = []
 
+        for num in nums:
+            total -= num      # right sum
+            ans.append(abs(left - total))
+            left += num       # update left sum
+
+        return ans
 
         
